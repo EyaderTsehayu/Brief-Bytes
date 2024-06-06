cddbe66feb503c7936e5df2f13c45e415ffea636
Eyader Tsehayu <eyaderts@gmail.com>
2024-06-06 20:22:35 +0300

    add landing page ui

---
+++ app/page.js
@@ -0,0 +1,20 @@
+import Feed from "@components/Feed";
+
+const Home = () => {
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center ">
+        EDiscover the Power of
+        <br className="max-md:hidden" />
+        <span className="violet_gradient text-center"> Concise Knowledge</span>
+      </h1>
+      <p className="desc text-center">
+        Empower your learning and share insights with Brief Byte, the platform
+        for crisp and impactful software engineering notes.
+      </p>
+      <Feed />
+    </section>
+  );
+};
+
+export default Home;
